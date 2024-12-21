297f257ff4b8f2f74cde5f75788f33423045a483
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-21 19:59:12 +0530

    added mainfont , added resuablity of svg component by implement interface type scripting , and added custom className props  ,to dynamically style the component

---
+++ src/components/Backgrounds/BackgroundGrid.tsx
@@ -0,0 +1,42 @@
+interface Background_Dynamics {
+  patternId?: string;
+  className?: string;
+}
+
+const BackgroundGrid = ({
+  patternId = "grid-pattern",
+  className = "",
+}: Background_Dynamics) => {
+  return (
+    <svg
+      className={`absolute inset-0 -z-10 h-full w-full stroke-gray-200 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)] ${className}`}
+      aria-hidden="true"
+    >
+      <defs>
+        <pattern
+          //   id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+          id={patternId}
+          width={200}
+          height={200}
+          x="50%"
+          y={-1}
+          patternUnits="userSpaceOnUse"
+        >
+          <path d="M.5 200V.5H200" fill="none" />
+        </pattern>
+      </defs>
+      <rect
+        // width="100%"
+        // height="100%"
+        // strokeWidth={0}
+        // fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
+        width="100%"
+        height="100%"
+        strokeWidth={0}
+        fill={`url(#${patternId})`}
+      />
+    </svg>
+  );
+};
+
+export default BackgroundGrid;
