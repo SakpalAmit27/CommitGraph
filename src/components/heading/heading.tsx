4524965b6956e61d5b725ce2254fe61d39b986cc
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-26 18:20:46 +0530

    creating heading component
    - will be reusable

---
+++ src/components/heading/heading.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+interface HeadingProps{
+    children:ReactNode
+    className?:string
+    
+}
+
+export const heading = ({children,className} : HeadingProps) => {
+
+}
