dd9c4064063cd2374e3902d66cf6f1e951fc93b8
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-26 17:12:08 +0530

    loaded the max-width-wrapper component in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 
 export default function Home() {
   return (
     <div className="flex items-center flex-col text-center">
 
-      <h2 className='font-mainfont font-bold text-4xl tracking-wide'>CommitGraph</h2>
+      {/* <h2 className='font-mainfont font-bold text-4xl tracking-wide'>CommitGraph</h2> */}
+
+      <MaxWidthWrapper>
+         <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+          <div>
+            <h1 className="font-mainfont font-bold text-4xl tracking-wide">CommitGraph</h1>
+          </div>
+         </div>
+      </MaxWidthWrapper>
+
     </div>
   );
 }
