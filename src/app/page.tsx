1074ee4b22c320dda932f0154a9548c87e17e5e8
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-27 14:36:22 +0530

    added
    - gradient look to the text
    
    - and some span tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,24 @@ import { MaxWidthWrapper } from "@/components/max-width-wrapper";
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col text-center">
+    <>
       {/* <h2 className='font-mainfont font-bold text-4xl tracking-wide'>CommitGraph</h2> */}
 
       <section className="relative py-24 sm:py-32 ">
         <MaxWidthWrapper className="text-center">
-          <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+          <div className="relative mx-auto text-center flex flex-col items-center gap-10 max-w-2xl">
             <div>
               <Heading>
-                Manage Your Complete Repositary Without Lazy Loadings
+                <span>Analyze Your Commits ,</span>
+                <br />
+                <span className="relative bg-gradient-to-r from-zinc-600 to-zinc-900 bg-clip-text text-transparent">
+                  Without Pressing Unnecessary Back Buttons !
+                </span>
               </Heading>
             </div>
           </div>
         </MaxWidthWrapper>
       </section>
-    </div>
+    </>
   );
 }
