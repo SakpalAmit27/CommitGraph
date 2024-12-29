0a0482f3c76c0bbec2e89add3aec6e2922c9365d
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-29 15:36:42 +0530

    added some long css
    - along with included the class Name resuability
    
    - and spreaded the props for extra resuablilty

--- src/components/ShinyButton/shiny-button.tsx
+++ src/components/ShinyButton/shiny-button.tsx
@@ -1,9 +1,15 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 import { AnchorHTMLAttributes } from "react";
 
 
-interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement>
+interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement>{
+
+}
 
 
 export const ShinyButton = ({className,children,href,...props} : ShinyButtonProps) => {
-
+    return <Link href={href ?? "#"} className={cn("group relative flex transfom items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-zinc-700 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-zinc-500 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",className)} {...props}>
+        <span></span>
+    </Link>
 }
