23a16026c8386172e6614634070955f12f8b114e
Sakpal Amit <amitsakpal744@gmail.com>
2024-12-29 15:24:51 +0530

    created the shiny-button component file
    -- added the interface
    
    -- and the function

---
+++ src/components/ShinyButton/shiny-button.tsx
@@ -0,0 +1,9 @@
+import { AnchorHTMLAttributes } from "react";
+
+
+interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement>
+
+
+export const ShinyButton = ({className,children,href,...props} : ShinyButtonProps) => {
+
+}
