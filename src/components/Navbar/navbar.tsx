69bb5a24d4cbc4c474844fa187b14a38cfe2f4b1
Sakpal Amit <amitsakpal744@gmail.com>
2025-01-03 19:57:46 +0530

    navbar progress
    -- added sticky
    -- backdrop blur lg
    -- and utilized the maxwidth wrapper
    
    for unified width on the landing page

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,8 +1,18 @@
+import { MaxWidthWrapper } from "../max-width-wrapper"
 
 
 export const Navbar = () => {
-    return <nav>
-        
+    // using sticky to make it fixed on the top // 
+    // inset-x-0 to spans from left to the right // 
+    // added backgrop-blur-lg to add that blurr effect behind // 
+
+
+    // using our maxwidhtwrapper for proper unified width of the landing page // 
+    return <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg">
+        <MaxWidthWrapper>
+            
+        </MaxWidthWrapper>
+
     </nav>
 
 }
