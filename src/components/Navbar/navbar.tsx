9e86810f654991c6f251960bc1051b2a8f9f618a
Sakpal Amit <amitsakpal744@gmail.com>
2025-01-03 20:32:07 +0530

    implemented the div along with Link which will be used as main logo

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,18 +1,19 @@
-import { MaxWidthWrapper } from "../max-width-wrapper"
-
+import Link from "next/link";
+import { MaxWidthWrapper } from "../max-width-wrapper";
 
 export const Navbar = () => {
-    // using sticky to make it fixed on the top // 
-    // inset-x-0 to spans from left to the right // 
-    // added backgrop-blur-lg to add that blurr effect behind // 
+  // using sticky to make it fixed on the top //
+  // inset-x-0 to spans from left to the right //
+  // added backgrop-blur-lg to add that blurr effect behind //
 
-
-    // using our maxwidhtwrapper for proper unified width of the landing page // 
-    return <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg">
-        <MaxWidthWrapper>
-            
-        </MaxWidthWrapper>
-
+  // using our maxwidhtwrapper for proper unified width of the landing page //
+  return (
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+            <Link href='/' className=""><span>CommitGraph</span></Link>
+        </div>
+      </MaxWidthWrapper>
     </nav>
-
-}
+  );
+};
