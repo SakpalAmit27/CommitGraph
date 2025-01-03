c81beb278836dd19e6e3ae7f263021490a0e6f1e
Sakpal Amit <amitsakpal744@gmail.com>
2025-01-03 12:52:15 +0530

    added animation to the github Icon
    -- used transition and transform
    
    -- added ease-in-out and group-hover to translate-x to go in right side

--- src/components/ShinyButton/shiny-button.tsx
+++ src/components/ShinyButton/shiny-button.tsx
@@ -1,15 +1,23 @@
 import { cn } from "@/lib/utils";
+import { Github } from "lucide-react";
 import Link from "next/link";
-import { AnchorHTMLAttributes } from "react";
+import { AnchorHTMLAttributes, ReactNode } from "react";
 
 
 interface ShinyButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement>{
+
+    children:ReactNode
+    className?:string
+    href?:string
 
 }
 
 
 export const ShinyButton = ({className,children,href,...props} : ShinyButtonProps) => {
-    return <Link href={href ?? "#"} className={cn("group relative flex transfom items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-zinc-700 px-8 text-base/7 font-medium text-white transition-all duration-300 hover:ring-2 hover:ring-zinc-500 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",className)} {...props}>
-        <span></span>
+    return <Link href={href ?? "#"} className={cn("group relative flex transfom items-center justify-center gap-2 overflow-hidden whitespace-nowrap rounded-md border border-white bg-zinc-900 px-8 text-base/7 font-semibold text-white transition-all duration-300 hover:ring-2 hover:ring-blue-500 hover:ring-offset-2 focus:outline-none focus:ring-2 focus:ring-offset-2",className)} {...props}>
+        <span className="relative z-10 flex items-center gap-2">
+            {children}
+            <Github className="size-6 shrink-0 text-white transition-transform duration-300 ease-in-out group-hover:translate-x-[7px]"/>
+        </span>
     </Link>
 }
