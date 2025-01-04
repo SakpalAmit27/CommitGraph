fcabe636464bca6050885eab3deec3a91d75e5da
Sakpal Amit <amitsakpal744@gmail.com>
2025-01-04 20:05:24 +0530

    preparing to create other sections of the navbar , along with some user validation , preparing for the pre authentication for signups or not signups show button

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -7,11 +7,16 @@ export const Navbar = () => {
   // added backgrop-blur-lg to add that blurr effect behind //
 
   // using our maxwidhtwrapper for proper unified width of the landing page //
+
+  const user = false
+
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg">
       <MaxWidthWrapper>
         <div className="flex h-16 items-center justify-between">
             <Link href='/' className=""><span className="font-mainfont text-2xl sm:text-3xl">CommitGraph</span></Link>
+
+            <div className="h-full items-center space-x-4"></div>
         </div>
       </MaxWidthWrapper>
     </nav>
